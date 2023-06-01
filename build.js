50a55b454b83eee1193190101e68bc8728f56c93
Tom Genoni <tom@genoni.dev>
2023-06-01 14:12:44 -0700

    Seeding with tokens and build script

---
+++ build.js
@@ -0,0 +1,22 @@
+const { registerTransforms } = require('@tokens-studio/sd-transforms');
+const StyleDictionary = require('style-dictionary');
+
+registerTransforms(StyleDictionary);
+
+const sd = StyleDictionary.extend({
+  source: ['src/**/tokens.json'],
+  platforms: {
+    css: {
+      buildPath: 'dist/css/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
+});
+
+sd.cleanAllPlatforms();
+sd.buildAllPlatforms();
